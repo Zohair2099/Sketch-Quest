e815b8082178c600ebcede527edc23328afcdd76
Vivotic zohair <Vivoticzohair@gmail.com>
2025-11-07 13:30:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8303454990-a2389",
+  "appId": "1:501648917241:web:ff6387843b860262e91828",
+  "apiKey": "AIzaSyCkfzebI81U6RR2JX6egnaQNaIw-Q20Dm8",
+  "authDomain": "studio-8303454990-a2389.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "501648917241"
+};
